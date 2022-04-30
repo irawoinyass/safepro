4ff5f81c7149609036b5d16b759c8aea908961aa
Lasisi Saheed <lasisisaheed5@gmail.com>
2022-04-30 12:57:41 +0100

    third

--- resources/views/MainInc/header.blade.php
+++ resources/views/MainInc/header.blade.php
@@ -11,8 +11,8 @@ Header Start  -->
 
                     <div class="header-menu d-none d-lg-block">
                         <ul class="main-menu">
-                            <li class="active-menu">
-                                <a class="active" href="/">Home</a>
+                            <li class="">
+                                <a href="/">Home</a>
                                 </li>
                             <li>
                                 <a href="/aboutus">Aboute Us</a>
@@ -36,7 +36,7 @@ Header Start  -->
                     <!-- Header Meta Start -->
                     <div class="header-meta">
                         <!-- Header Info Start -->
-                        <div class="header-info d-none d-md-flex">
+                     <!--    <div class="header-info d-none d-md-flex">
                             <div class="info-icon">
                                 <i class="fas fa-phone-alt"></i>
                             </div>
@@ -44,7 +44,7 @@ Header Start  -->
                                 <span>Free Consultant</span>
                                 <span class="number"><a href="tel:+44 7546 672405">+44 7546 672405</a></span>
                             </div>
-                        </div>
+                        </div> -->
                         <!-- Header Info End -->
                         <!-- Header Search Start -->
                       <!--   <div class="header-search">
@@ -116,38 +116,25 @@ Header Start  -->
             <div class="offcanvas-body">
                 <div class="offcanvas-menu">
                     <ul class="main-menu">
-                        <li class="active-menu">
-                            <a href="index.html">Home</a>
-                            <ul class="sub-menu">
-                                <li class="active"><a href="index.html">Home One</a></li>
-                                <li><a href="index-2.html">Home Two</a></li>
-                                <li><a href="index-3.html">Home Three</a></li>
-                                <li><a href="index-4.html">Home Four</a></li>
-                                <li><a href="index-5.html">Home Five</a></li>
-                                <li><a href="index-6.html">Home Six</a></li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a href="about.html">Aboute Us</a>
-                        </li>
-                        <li><a href="#">Pages</a>
-                            <ul class="sub-menu">
-                                <li><a href="team.html">Our Team</a></li>
-                                <li><a href="service.html">Service</a></li>
-                                <li><a href="why-choose.html">Why Choose Us</a></li>
-                                <li><a href="testimonial.html">Testimonial</a></li>
-                                <li><a href="pricing.html">Pricing</a></li>
-                                <li><a href="login-register.html">Login & Register</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Blog</a>
-                            <ul class="sub-menu">
-                                <li><a href="blog.html">Blog Grid</a></li>
-                                <li><a href="blog-standard.html">Latest News</a></li>
-                                <li><a href="blog-details.html">Blog Details</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="contact.html">Contact</a></li>
+                       <li class="">
+                                <a href="/">Home</a>
+                                </li>
+                            <li>
+                                <a href="/aboutus">Aboute Us</a>
+                            </li>
+                            <li><a href="javascript:void(0);">Services</a>
+                                <ul class="sub-menu">
+                                    <li><a href="/Information security and assurance">Information security and assurance</a></li>
+                                    <li><a href="/Technology advisory and consulting">Technology advisory and consulting</a></li>
+                                    <li><a href="/Products and solution design">Products and solution design</a></li>
+                                    <li><a href="/Networking and cloud support">Networking and cloud support</a></li>
+            
+                                </ul>
+                            </li>
+                            <li><a href="/careers">Careers</a>
+                                
+                            </li>
+                            <li><a href="/contactus">Contact</a></li>
                     </ul>
                 </div>
             </div>
